fad539e703d31be8ed608561a3ec1afe8e379d2b
Ananyahs <39176552+Ananyahs@users.noreply.github.com>
2023-06-26 12:27:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+    window.onload = function() {
+      const browserInfo = getBrowserInfo();
+      displayMessage(browserInfo);
+    };
+
+    function getBrowserInfo() {
+      const browserName = navigator.appName;
+      const browserVersion = navigator.appVersion;
+      return {
+        name: browserName,
+        version: browserVersion
+      };
+    }
+
+    function displayMessage(browserInfo) {
+		
+      const message = "You are using " + browserInfo.name + " " + browserInfo.version;
+      document.getElementById("browser-info").innerHTML = message;
+    }
+  
